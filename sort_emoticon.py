ec4872266ee6c2aacca049e5e324f56c823bf578
tiwanari <tatsuyaw0c@gmail.com>
2019-10-23 22:33:46 +0900

    sort_emoticon.py のリファクタリング

--- sort_emoticon.py
+++ sort_emoticon.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 
 if __name__ == '__main__':
+    filename = 'source.txt'
     data = []
-    for line in open('emoticon.txt', 'r'):
-        data += [line[:-1].split('\t')]
+
+    with open(filename, 'r') as f:
+        for line in f:
+            data += [line[:-1].split('\t')]
 
     data = sorted(data, key=lambda x: x[0])
 
-    g = open('emoticon.txt', 'w')
-    for row in data:
-        g.write(row[0] + '\t' + row[1] + '\t' + row[2] + '\n')
+    with open(filename, 'w') as f:
+        for row in data:
+            f.write(row[0] + '\t' + row[1] + '\t' + row[2] + '\n')
