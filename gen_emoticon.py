47293b00f05d5d672804d05f0f6f96b931e1e83b
tiwanari <tatsuyaw0c@gmail.com>
2019-10-23 22:36:09 +0900

    sort_emoticon.py -> gen_emoticon.py へリネーム

--- gen_emoticon.py
+++ gen_emoticon.py
@@ -1,15 +1,16 @@
 # coding: utf-8
 
 if __name__ == '__main__':
-    filename = 'source.txt'
-    data = []
+    src = 'source.txt'
+    dst = 'emoticon.txt'
 
-    with open(filename, 'r') as f:
+    data = []
+    with open(src, 'r') as f:
         for line in f:
             data += [line[:-1].split('\t')]
 
     data = sorted(data, key=lambda x: x[0])
 
-    with open(filename, 'w') as f:
+    with open(dst, 'w') as f:
         for row in data:
             f.write(row[0] + '\t' + row[1] + '\t' + row[2] + '\n')
